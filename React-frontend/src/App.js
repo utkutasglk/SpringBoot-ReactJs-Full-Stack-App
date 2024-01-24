daa89a12caa3b445eff395f3d555538e2b544aa2
utkutasglk <utku.tasguluk@gmail.com>
2024-01-24 22:37:07 +0300

    Home.js, User.js, Navbar.js were created.

--- React-frontend/src/App.js
+++ React-frontend/src/App.js
@@ -1,8 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavBar></NavBar>
+        <Switch>
+          <Route exact path="/" Component={Home}></Route>
+          <Route exact route="/users/:userId">
+            component={User}
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
